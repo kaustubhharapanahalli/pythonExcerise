961bd73206c96e42f28edfec05c725488835b4ee
Jillala Srinivas Reddy <srinivasreddy.j@gmail.com>
2017-12-20 17:09:03 +0800

    lambda example

---
+++ lambdaEx.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+
+def make_incrementor(n):
+    return lambda x: x + n
+
+f = make_incrementor(2)
+g = make_incrementor(6)
+
+print f(42), g(42)
+print make_incrementor(23)(22)
